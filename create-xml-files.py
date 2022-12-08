b25402798e89654ec69281d793aad990048e1000
Ivan Averin <i.averin@gmail.com>
2022-12-08 17:16:47 +0300

    refactor: removed global constants from xml generating functions. renamed constant for max objects in xml

--- create-xml-files.py
+++ create-xml-files.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as ET
 import zipfile
 from typing import Callable
 
-XML_MAX_OBJECTS = 10 
+MAX_OBJECTS_IN_XML = 10 
 XML_FILES_IN_ZIP = 100
 ZIP_FILES = 50 
 ZIP_DIRECTORY = "zip-files"
@@ -14,18 +14,18 @@ ZIP_DIRECTORY = "zip-files"
 def random_string() -> str:
     return str(uuid.uuid4())
 
-def create_objects() -> ET.Element:
-    objects_num = random.randrange(1, XML_MAX_OBJECTS+1)
+def create_objects(max_objects_in_xml: int) -> ET.Element:
+    objects_num = random.randrange(1, max_objects_in_xml+1)
     objects = ET.Element("objects")
     for i in range(0, objects_num):
         objects.append(ET.Element("object", {"name": random_string()}))
     return objects    
 
-def create_xml_tree() -> ET.ElementTree:
+def create_xml_tree(objects_in_xml: int) -> ET.ElementTree:
     root = ET.Element("root")
     root.append(ET.Element("var", attrib={"name":"id", "value" : random_string()}))
     root.append(ET.Element("var", attrib={"name":"level", "value" : str(random.randrange(1,101))}))
-    root.append(create_objects())
+    root.append(create_objects(objects_in_xml))
     tree = ET.ElementTree(root)
     ET.indent(tree)
     return tree
@@ -48,13 +48,13 @@ def create_dir(dir) -> bool:
         print(f"Could not create directory {dir}. \n Error: {e}")
         return False
 
-def create_zip_files(xml_files_in_zip: int, number_zip_files: int,  zip_files_dir: int, log_created_file: Callable = None) -> int:
+def create_zip_files(objects_in_xml, xml_files_in_zip: int, number_zip_files: int,  zip_files_dir: int, log_created_file: Callable = None) -> int:
     zip_files_created = 0
     for zip_file_index in range(1,number_zip_files+1):
         zip_file_name = f"{zip_files_dir}/{str(zip_file_index)}.zip"
         with zipfile.ZipFile(zip_file_name,"w") as z:
             for i in range (1, xml_files_in_zip+1):
-                z.writestr(zipfile.ZipInfo(f"{i}.xml"), generate_xml_file_data(create_xml_tree()))
+                z.writestr(zipfile.ZipInfo(f"{i}.xml"), generate_xml_file_data(create_xml_tree(objects_in_xml)))
             zip_files_created += 1
         if log_created_file:
             log_created_file(f"{zip_file_name}")
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     
     number_of_created_zip_files = 0
     try:
-        number_of_created_zip_files = create_zip_files(XML_FILES_IN_ZIP, ZIP_FILES, ZIP_DIRECTORY, lambda x: print(f"Created zip file {x}"))
+        number_of_created_zip_files = create_zip_files(MAX_OBJECTS_IN_XML, XML_FILES_IN_ZIP, ZIP_FILES, ZIP_DIRECTORY, lambda x: print(f"Created zip file {x}"))
     except OSError as e:
             print(f"Error saving zip file. \n Error: {e}")
     
