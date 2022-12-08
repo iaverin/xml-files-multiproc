c2f63360edb350a6a270118f7161fef781cadf78
Ivan Averin <i.averin@gmail.com>
2022-12-08 18:55:18 +0300

    create package structure

---
+++ ngxmlzip/process.py
@@ -0,0 +1,4 @@
+def process(x):
+    print("ok")
+
+
---
+++ run.py
@@ -0,0 +1,8 @@
+import ngxmlzip.process as process
+from ngxmlzip.create import ZIP_DIRECTORY
+
+if __name__ == "__main__":
+    print(ZIP_DIRECTORY)
+    process.process(1)
+
+
