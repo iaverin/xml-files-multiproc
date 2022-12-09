f45a5744793dc89aba7a3503892a66b566f831d1
Ivan Averin <i.averin@gmail.com>
2022-12-09 03:57:29 +0300

    removed reopening zip file for every xml added run function and profiler

--- ngxmlzip/process.py
+++ ngxmlzip/process.py
@@ -7,7 +7,8 @@ import io
 import os
 from dataclasses import dataclass
 import csv
-
+import cProfile
+import pstats
 
 def process(x):
     print("ok")
@@ -68,47 +69,58 @@ def parse_xml_file(xml_file_data) -> ParsedXMLData:
     return parsed_data
 
 
-def create_csv_file_type_1(csv_file: str, delimiter=','):
+def create_csv_file_type_1(csv_file: str, delimiter=","):
     with open(csv_file, "w", newline="") as csvfile:
         writer = csv.writer(csvfile, delimiter=delimiter)
         writer.writerow(["id", "level"])
 
 
-def create_csv_file_type_2(csv_file: str, delimiter=','):
+def create_csv_file_type_2(csv_file: str, delimiter=","):
     with open(csv_file, "w", newline="") as csvfile:
         writer = csv.writer(csvfile, delimiter=delimiter)
         writer.writerow(["id", "object_name"])
 
 
-def append_csv_file_type_1(csv_file: str, data: ParsedXMLData, delimiter =","):
+def append_csv_file_type_1(csv_file: str, data: ParsedXMLData, delimiter=","):
     with open(csv_file, "a", newline="") as csvfile:
         writer = csv.writer(csvfile, delimiter=delimiter)
         writer.writerow([data.id, data.level])
 
-def append_csv_file_type_2(csv_file: str, data: ParsedXMLData, delimiter =","):
+
+def append_csv_file_type_2(csv_file: str, data: ParsedXMLData, delimiter=","):
     with open(csv_file, "a", newline="") as csvfile:
         writer = csv.writer(csvfile, delimiter=delimiter)
         for object_name in data.object_names:
             writer.writerow([data.id, object_name])
 
+def run():
+    if os.path.split(os.getcwd())[1].split(os.sep)[-1] == "ngxmlzip":
+        zip_dir = f"../{ZIP_DIRECTORY}"
+    else:
+        zip_dir = f"{ZIP_DIRECTORY}"
 
-if os.path.split(os.getcwd())[1].split(os.sep)[-1] == "ngxmlzip":
-    zip_dir = f"../{ZIP_DIRECTORY}"
-else:
-    zip_dir = f"{ZIP_DIRECTORY}"
+    create_csv_file_type_1("csv_file_1.csv")
+    create_csv_file_type_2("csv_file_2.csv")
 
-create_csv_file_type_1("csv_file_1.csv")
-create_csv_file_type_2("csv_file_2.csv")
-
-zip_files = get_zip_files(f"{zip_dir}/*.zip")
-for zip_file in zip_files:
-    xml_files = get_xml_files(zip_file)
-    for xml_file in xml_files:
-        print(f"Zip file {zip_file}. XML file {xml_file}")
+    zip_files = get_zip_files(f"{zip_dir}/*.zip")
+    for zip_file in zip_files:
         with zipfile.ZipFile(zip_file, mode="r") as zip:
-            xml_data = xml_from_zip(zip, xml_file)
-            parsed_xml_data = parse_xml_file(xml_data)
-            # print("XML data", parsed_xml_data)
+            xml_files = get_xml_files(zip_file)
+            for xml_file in xml_files:
+                print(f"Zip file {zip_file}. XML file {xml_file}")
+                xml_data = xml_from_zip(zip, xml_file)
+                parsed_xml_data = parse_xml_file(xml_data)
+                # print("XML data", parsed_xml_data)
+                append_csv_file_type_1("csv_file_1.csv", parsed_xml_data)
+                append_csv_file_type_2("csv_file_2.csv", parsed_xml_data)
 
-            append_csv_file_type_1("csv_file_1.csv", parsed_xml_data)
-            append_csv_file_type_2("csv_file_2.csv", parsed_xml_data)
+if __name__ == "__main__":
+    # cProfile.run('run()')
+    profiler = cProfile.Profile()
+    profiler.enable()
+    run()
+    profiler.disable()
+    stats = pstats.Stats(profiler).sort_stats('cumtime')
+    stats.print_stats()
+
+    
